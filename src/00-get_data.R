2b5ba61fb47d0b7cd7e1f20e1d1f3fe9cb3292fc
Jose Manuel Aburto <jm.aburto01@gmail.com>
2021-11-09 15:05:17 +0000

    Some sensitivity checks and plots

---
+++ src/00-get_data.R
@@ -0,0 +1,119 @@
+# Get data in functional form
+
+# Init ------------------------------------------------------------
+library(here); library(glue)
+library(tidyverse); library(yaml)
+library(patchwork); library(data.table)
+library(gt)
+library(haven)
+
+# Constants -------------------------------------------------------
+
+wd <- here()
+cnst <- list()
+config <- read_yaml(glue('{wd}/cfg/config.yaml'))
+
+cnst <- within(cnst, {
+  regions_for_analysis = config$regions_for_all_cause_analysis
+  path_out = glue('{wd}/out')
+  path_tmp = glue('{wd}/tmp')
+  # number of Poisson life-table replicates
+  n_sim = 500
+  min_age = 15
+  var_names = c('age','year','sex','reth','deaths','pop','obesity',
+                'respiratory','infectious','drug',
+                'alcohol','suicide','female_cancer','male_cancer','rest_cancers',
+                'accidents','rest')
+})
+
+dat <- list()
+fig <- list()
+tab <- list()
+
+# Read data --------------------------------------------------------
+
+##Pop	population count in race/ethnic, sex, year group	
+#Mort	total number of deaths  in race/ethnic, sex, year group	
+#Reth	race/ethnic group variable, should have labels (1 "NHW" 2 "NHB" 3 "LatinX" 4 "AI/AN" 5 "Other")	
+#Male	1=male, 0=female	
+
+
+#some checks prior
+
+dat$analytic <- data.table(read_dta(glue('{wd}/dat/analytic.dta')))
+
+#view(dat$analytic)
+
+dat$masters <- dat$analytic[,c('age','year','male','reth','mort','pop','m1',
+                               'm2','m3','drug','alch','sui','fgs_cancers',
+                               'mgs_cancers','mostcancers','m6','m7')]
+
+names(dat$masters) <- cnst$var_names
+
+dat$masters$scheme <- 'masters'
+
+dat$acosta <- dat$analytic[,c('age','year','male','reth','mort','pop','ac1',
+                               'ac2','ac3','drug','alch','sui','fgs_cancers_acosta',
+                               'mgs_cancers_acosta','mostcancers_acosta','ac6','ac7')]
+
+names(dat$acosta) <- cnst$var_names
+
+dat$acosta$scheme <- 'acosta'
+
+dat$adair <- dat$analytic[,c('age','year','male','reth','mort','pop','ad1',
+                               'ad2','ad3','drug','alch','sui','fgs_cancers',
+                               'mgs_cancers','mostcancers','ad6','ad7')]
+
+names(dat$adair) <- cnst$var_names
+
+dat$adair$scheme <- 'adair'
+
+dat$gbd <- dat$analytic[,c('age','year','male','reth','mort','pop','gbd1',
+                             'gbd2','gbd3','drug','alch','sui','fgs_cancers_gbd',
+                             'mgs_cancers_gbd','mostcancers_gbd','gbd6','gbd7')]
+
+names(dat$gbd) <- cnst$var_names
+
+dat$gbd$scheme <- 'gbd'
+
+dat$ucod <- dat$analytic[,c('age','year','male','reth','mort','pop','o1',
+                           'o2','o3','drug','alch','sui','fgs_cancers',
+                           'mgs_cancers','mostcancers','o6','o7')]
+
+names(dat$ucod) <- cnst$var_names
+
+dat$ucod$scheme <- 'ucod'
+
+## bind them to have only one dataset
+
+dat$analytic_bind <- rbind(dat$masters,dat$acosta,dat$adair,dat$gbd,dat$ucod)
+#view(dat$analytic_bind)
+
+#check death rates, mort >  pop, consistency with totals, etc.
+
+dat$analytic_bind[deaths > pop]
+
+
+#move to long format, easier to calculate proportions
+
+dat$deaths_input_100 <- melt.data.table(data = dat$analytic_bind,
+                                          id.vars = c('age','year','sex','reth','scheme','deaths','pop'),
+                                          variable.name = 'cause',value.name = 'deaths_cause')
+
+#check for NA's
+
+#view(check_NA)
+dat$deaths_input_100[is.na(dat$deaths_input_100$deaths)]$deaths_cause <- 0
+dat$deaths_input_100[is.na(dat$deaths_input_100$deaths)]$deaths <- 0
+
+check_NA <- dat$deaths_input_100[is.na(dat$deaths_input_100$deaths)]
+check_NA
+
+#check (cause-specific) = total
+sum(dat$deaths_input_100[,sum(deaths_cause) - deaths[1], by = .(age,year,sex,reth,scheme)]$V1)
+
+
+#save dataset
+saveRDS(dat$deaths_input_100,glue('{cnst$path_out}/deaths_input_100.rds'))
+
+
---
+++ src/01a-mx_description_COD_proporitons.R
@@ -0,0 +1,152 @@
+# General life-table analysis
+
+# Init ------------------------------------------------------------
+
+set.seed(1987)
+
+library(here); library(glue)
+library(tidyverse); library(yaml)
+library(patchwork)
+library(gt); library(openxlsx)
+
+# Constants -------------------------------------------------------
+
+wd <- here()
+cnst <- list()
+config <- read_yaml(glue('{wd}/cfg/config.yaml'))
+region_meta <- read_csv(glue('{wd}/cfg/region_metadata.csv'), na = '.')
+cnst <- within(cnst, {
+  regions_for_analysis = config$regions_for_all_cause_analysis
+  path_out = glue('{wd}/out')
+  path_tmp = glue('{wd}/tmp')
+  # number of Poisson life-table replicates
+  n_sim = 500
+})
+
+dat <- list()
+fig <- list()
+tab <- list()
+
+
+
+# functions ----------------------------------------------------------------
+
+CI_s_mx <- function(dt = .SD, n_sim = cnst$n_sim){
+  deaths_sim <- rpois(lambda = dt$deaths,n = n_sim)
+  deaths_low <- quantile(deaths_sim, 0.025, na.rm = TRUE)
+  deaths_high <- quantile(deaths_sim, 0.975, na.rm = TRUE)
+  
+  mx_low <- deaths_low/dt$pop
+  mx_high <- deaths_high/dt$pop
+  mx <-  dt$deaths/dt$pop
+  
+  return(data.table(mx,mx_low,mx_high))
+}
+
+
+
+# Data ------------------------------------------------------------
+
+# input data for life-table calculation
+# harmonized death counts and population exposures with open age group 100+
+dat$deaths_input_100 <- readRDS(glue('{cnst$path_out}/deaths_input_100.rds'))
+
+#create a dataset with total deaths only 
+dat$mx_input_100 <- dat$deaths_input_100[scheme %in% 'masters' & cause %in% 'obesity',.(deaths = deaths, pop = pop), by = .(reth,year,sex,age)]
+
+# Description of mx -------------------------------------------------------
+
+#calculate mx
+dat$mx_input_100 <- dat$mx_input_100[, CI_s_mx(dt = .SD,n_sim = cnst$n_sim),by = .(reth,year,sex,age)]
+dat$mx_input_100[,nx := 1]
+dat$mx_input_100[age == 100,nx := Inf]
+
+#recode some variables
+#Reth	race/ethnic group variable, should have labels (1 "NHW" 2 "NHB" 3 "LatinX" 4 "AI/AN" 5 "Other")	
+#Male	1=male, 0=female	
+dat$mx_input_100[, reth:= factor(x = reth,labels = c('nhw','nhb','latinx','aian','other'))]
+dat$mx_input_100[, sex:= factor(x = sex,labels = c('female','male'))]
+
+
+# Diagnostic plots of mx --------------------------------------------------
+
+fig$fig_mx <- ggplot(data = dat$mx_input_100[year %in% c(2010,2015,2019)]) +
+  ggtitle('mx by year, sex and subgroup')+
+  geom_ribbon(
+    aes(
+      x = age,
+      ymin = log(mx_low),
+      ymax = log(mx_high),
+      fill = sex,
+      group = sex
+    ),
+    alpha = 0.3
+  ) +
+  geom_line(
+    aes(x = age, y = log(mx), color = sex, group = sex)
+  ) +
+  geom_point(
+    aes(x = age, y = log(mx), color = sex,group = sex),
+    size = 1
+  ) +
+  labs(
+    x = 'Age',
+    y = 'mx'
+  ) +
+  facet_grid(reth~year)
+
+fig$fig_mx
+  
+ggsave(glue('{cnst$path_out}/fig_mx.png'),plot = fig$fig_mx,width = 10,height = 10)
+
+#NTS: need to potentially take care of aian and other using some modelling strategy
+
+
+# Diagnostic plots of COD --------------------------------------------------
+
+dat$deaths_input_100[, reth:= factor(x = reth,labels = c('nhw','nhb','latinx','aian','other'))]
+dat$deaths_input_100[, sex:= factor(x = sex,labels = c('female','male'))]
+dat$deaths_input_100[, cause:= as.factor(cause)]
+dat$deaths_input_100[, scheme:= as.factor(scheme)]
+dat$deaths_input_100[, deaths_cause_prop:= deaths_cause/deaths]
+
+
+fig$fig_COD_prop_females <- 
+ggplot(data = dat$deaths_input_100[sex == 'female' & year == 2019],
+        aes(
+          x = age,
+          y = deaths_cause_prop,
+          fill = cause)) +
+  ggtitle('COD structure over time, scheme and subgroup, females')+
+  geom_area(stat = 'identity', position = 'fill')+
+  labs(
+    x = 'Age',
+    y = 'Proportion'
+  ) +
+  facet_grid(reth~scheme)+
+   scale_fill_manual(values = c(1,rep(2,10)))
+
+
+fig$fig_COD_prop_males <- 
+  ggplot(data = dat$deaths_input_100[sex == 'male' & year == 2019],
+         aes(
+           x = age,
+           y = deaths_cause_prop,
+           fill = cause)) +
+  ggtitle('COD structure over time, scheme and subgroup, males')+
+  geom_area(stat = 'identity', position = 'fill')+
+  labs(
+    x = 'Age',
+    y = 'Proportion'
+  ) +
+  facet_grid(reth~scheme)+
+  scale_fill_manual(values = c(1,rep(3,10)))
+
+
+
+ggsave(glue('{cnst$path_out}/fig_cod_prop_females.png'),plot = fig$fig_COD_prop_females,width = 10,height = 10)
+
+
+ggsave(glue('{cnst$path_out}/fig_cod_prop_males.png'),plot = fig$fig_COD_prop_males,width = 10,height = 10)
+
+
